79669643ac0b55e762b5ab8f654b355eda75d46c
Diego Medina <diego@fmpwizard.com>
2012-03-22 05:59:52 -0400

    * Deleted pom.xml, we are using sbt here * Added the log file src/main/resources/props/default.logback.xml * See how we added the logback library in project/build/LiftProject.scala

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,9 +1,24 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val liftVersion = "2.4"
 
-  // Add Maven Local repository for SBT to search for (disable if this doesn't suit you)
-  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+  // uncomment the following if you want to use the snapshot repo
+  //  val scalatoolsSnapshot = ScalaToolsSnapshots
 
-  
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
+
+  lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
+    "junit" % "junit" % "4.7" % "test",
+    "ch.qos.logback" % "logback-classic" % "0.9.26",
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
+    "com.h2database" % "h2" % "1.2.147"
+  ) ++ super.libraryDependencies
 }
